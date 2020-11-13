84ef730e40a42197b7953cb10c462de87386307b
Toby Dragon <tdragon@ithaca.edu>
2020-11-13 09:58:23 -0500

    added comments

--- src/main/java/edu/ithaca/dragon/datastructures/tree/BTNodeUtil.java
+++ src/main/java/edu/ithaca/dragon/datastructures/tree/BTNodeUtil.java
@@ -2,10 +2,14 @@ package edu.ithaca.dragon.datastructures.tree;
 
 public class BTNodeUtil {
 
+    //the number of edges on the longest path in the tree
     public static int height(BTNode root){
         return -1;
     }
 
+    // add a new node in the tree, such that the tree stays roughly balanced
+    // if the height of the left subtree is greater than the right, add somewhere right
+    // else add somewhere left
     public static void add(BTNode root, int newKey){
 
     }
